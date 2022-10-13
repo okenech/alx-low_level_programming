a0733386b4e1d693ae408e39602090ea7fde19ee
Kenechukwu Okafor <okaforkene2@gmail.com>
2022-10-13 15:57:28 -0700

    added question 3 & $

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 
+/**
+ * main - prints "Programming is like building a muiltilingual puzzle
+ * Return: 0 if exited
+ */
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
